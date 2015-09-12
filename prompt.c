0d0188f41173e20c5dacaa5342846f7ef03b42a2
Ernesto Celis <ernesto@celisdelafuente.net>
2015-09-12 02:36:06 +0000

    Added windows support

--- prompt.c
+++ prompt.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+/* Win support */
+#ifdef _WIN32
+
+/* Fake readline */
+char* readline(char* prompt)
+{
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer) + 1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
+}
+
+/* Fake add_history */
+void add_history(char* unused) {}
+
+/* Add Linux readline suuport */
+#else
 #include <readline/readline.h>
 #include <readline/history.h>
+#endif
 
 int main(int argc, char** argv)
 {
   /* Print version and Exit information */
-  puts("Lezchty Version 0.0.1");
+  puts("Lezchty Version 0.0.2");
   puts("(C) 2015 Ernesto Celis");
   puts("Press Ctrl+c to Exit\n");
 
@@ -19,7 +40,7 @@ int main(int argc, char** argv)
     add_history(input);
 
     /* Echo input back to user */
-    printf("No you're a %s\n", input);
+    printf("=>%s\n", input);
 
     /* Free retrieved input */
     free(input);
