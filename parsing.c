c6fd39e874d0564199971ce0201f202635cde597
Ernesto Celis <ernesto@celisdelafuente.net>
2015-09-13 22:04:42 +0000

    BROKEN WIP S-Expressions

--- parsing.c
+++ parsing.c
@@ -32,31 +32,154 @@ void add_history(char *unused) {}
 enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
 
 /* lval Types */
-enum { LVAL_NUM, LVAL_ERR };
+enum { LVAL_NUM, LVAL_ERR, LVAL_SYM, LVAL_SEXPR };
 
 /* lval */
-typedef struct {
+typedef struct lval {
   int type;
   long num;
-  int err;
+  /* Error and Symbol have string data */
+  char *err;
+  char *sym;
+  /* Count and Pointer to a list of lval */
+  int count;
+  struct lval **cell;
 } lval;
 
+/* Forward define functions */
+void lval_println(lval *v);
+
 /* lval Number Type */
-lval lval_num(long x)
+lval *lval_num(long x)
 {
-  lval v;
-  v.type = LVAL_NUM;
-  v.num = x;
+  lval *v malloc(sizeof(lval));
+  v->type = LVAL_NUM;
+  v->num = x;
   return v;
 }
 
 /* lval Error Type */
-lval lval_err(int x)
+lval *lval_err(char *m)
 {
-  lval v;
-  v.type = LVAL_ERR;
-  v.err = x;
+  lval *v = malloc(sizeof(lval));
+  v->type = LVAL_ERR;
+  v->err = malloc(strlen(m) + 1);
+  strcpy(v->err, m);
   return v;
+}
+
+/* lval Symbol */
+lval *lval_sym(char *s)
+{
+  lval *v = malloc(sizeof(lval));
+  v->type = LVAL_SYM;
+  v->sym = malloc(strlen(s) + 1);
+  strcpy(v->sym, s);
+  return v;
+}
+
+/* Empty lval Sexpr */
+lval *lval_sexpr(void)
+{
+  lval *v = malloc(sizeof(lval));
+  v->type = LVAL_SEXPR;
+  v->count = 0;
+  v->cell = NULL;
+  return v;
+}
+
+void lval_del(lval *v)
+{
+  switch (v->type) {
+    case LVAL_NUM:
+      break;
+    case LVAL_ERR:
+      free(v->err);
+      break;
+    case LVAL_SYM:
+      free(v->sym);
+      break;
+    case LVAL_SEXPR:
+        for (int i = 0; i < v->count; i++) {
+          lval_del(v->cell[i]);
+        }
+      free(v->cell);
+      break;
+  }
+}
+
+lval *lval_read_num(mpc_ast_t *t)
+{
+  errno = 0;
+  long x = strtol(t->contents, NULL, 10);
+  return errno != ERANGE ? lval_num(x) : lval_err("Invalid number");
+}
+
+lval *lval_read(mpc_ast_t *t)
+{
+  if (strstr(t->tag, "number")) { return lval_read_num(t); }
+  if (strstr(t->tag, "symbol")) { return lval_sym(t->contents); }
+
+  /* If root (>) or sexpr create empty list */
+  lval *x = NULL;
+  if (strcmp(t->tag, ">") == 0) { x = lval_sexpr(); }
+  if (strcmp(t->tag, "sexpr")) { x = lval_sexpr(); }
+
+  /* Fill the list with any valid expression contained within */
+  for (int i = 0; i < t->children_num; i++) {
+    if (strcmp(t->children[i]->contents, "(") == 0) { continue; }
+    if (strcmp(t->children[i]->contents, ")") == 0) { continue; }
+    if (strcmp(t->children[i]->contents, "}") == 0) { continue; }
+    if (strcmp(t->children[i]->contents, "{") == 0) { continue; }
+    if (strcmp(t->children[i]->tag, "regex") == 0) { continue; }
+    x = lval_add(x, lval_read(t->children[i]));
+  }
+
+  return x;
+}
+
+lval *lval_add(lval *v, lval *x)
+{
+  v->count++;
+  v->cell = realloc(v->cell, sizeof(lval*) * v->count);
+  v->cell[v->count-1] = x;
+  return v;
+}
+
+void lval_expr_print(lval *v, char open, char close)
+{
+  putchar(open);
+  for (int i = 0; i < v->count; i++) {
+    lval_print(v->cell[i]);
+    if(i != (v->count-1)) {
+      putchar(' ');
+    }
+  }
+  putchar(close);
+}
+
+void lval_print(lval *v)
+{
+  switch (v->type) {
+    case LVAL_NUM:
+      printf("%li", v->num);
+      break;
+    case LVAL_ERR:
+      printf("Error: %s", v->err);
+      break;
+    case LVAL_SYM:
+      printf("%s", v->sym);
+      break;
+    case LVAL_SEXPR:
+      lval_expr_print(v, '(', ')');
+      break;
+  }
+}
+
+void lval_println(lval *v)
+{
+  lval_print(v);
+  putchar('\n');
 }
 
 /* Print an lval */
@@ -132,8 +255,8 @@ int main(int argc, char** argv)
 {
   /* Create parsers */
   mpc_parser_t *Number    = mpc_new("number");
-  mpc_parser_t *Letter    = mpc_new("letter");
-  mpc_parser_t *Operator  = mpc_new("operator");
+  mpc_parser_t *Symbol    = mpc_new("symbol");
+  mpc_parser_t *Sexpr     = mpc_new("sexpr");
   mpc_parser_t *Expr      = mpc_new("expr");
   mpc_parser_t *Lezchty   = mpc_new("lezchty");
 
@@ -141,12 +264,12 @@ int main(int argc, char** argv)
   mpca_lang(MPCA_LANG_DEFAULT,
     "                                                                 \
       number    :  /-?[0-9]+/ ;                                       \
-      letter    :  'a' | 'b'  ;                                       \
-      operator  :  '+' | '-' | '*' | '/' | '%' ;                      \
-      expr      : <number> | <letter> | '(' <operator> <expr>+ ')' ;  \
-      lezchty   : /^/ <operator> <expr>+ /$/ ;                        \
+      symbol  :  '+' | '-' | '*' | '/' | '%' ;                      \
+      sexpr    :  '(' <expr>* ')' ;                                       \
+      expr      : <number> | <letter> | '(' <symbol> <expr>+ ')' ;  \
+      lezchty   : /^/ <symbol> <expr>+ /$/ ;                        \
     ",
-    Number, Letter, Operator, Expr, Lezchty);
+    Number, Symbol, Sexpr, Expr, Lezchty);
 
 
   /* Print version and Exit information */
@@ -178,7 +301,7 @@ int main(int argc, char** argv)
   }
 
   /* Undefine and Delete Parsers */
-  mpc_cleanup(5, Number, Letter, Operator, Expr, Lezchty);
+  mpc_cleanup(5, Number, Symbol, Sexpr, Expr, Lezchty);
 
   return 0;
 }
