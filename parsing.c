4c36fd5c7bc8c0e732d55bde3209bf305afb36c4
Ernesto Celis <ernesto@celisdelafuente.net>
2015-09-13 20:43:59 +0000

    Basic arithmetic evaluation

--- parsing.c
+++ parsing.c
@@ -28,23 +28,58 @@ void add_history(char *unused) {}
 #include <readline/history.h>
 #endif
 
+/* Use operator string to see which operation perform */
+long eval_op(long x, char *op, long y)
+{
+  if (strcmp(op, "+") == 0) { return x + y; }
+  if (strcmp(op, "-") == 0) { return x - y; }
+  if (strcmp(op, "*") == 0) { return x * y; }
+  if (strcmp(op, "/") == 0) { return x / y; }
+  return 0;
+}
+
+
+long eval(mpc_ast_t *t)
+{
+  if (strstr(t->tag, "number")) {
+    return atoi(t->contents);
+  }
+
+  /* Operator is always second child */
+  char *op = t->children[1]->contents;
+
+  /* Store third child in `x` */
+  long x = eval(t->children[2]);
+
+  /* Iterate remaining childre and combining */
+  int i = 3;
+  while (strstr(t->children[i]->tag, "expr")) {
+    x = eval_op(x, op, eval(t->children[i]));
+    i++;
+  }
+
+  return x;
+}
+
 int main(int argc, char** argv)
 {
   /* Create parsers */
   mpc_parser_t *Number    = mpc_new("number");
+  mpc_parser_t *Letter    = mpc_new("letter");
   mpc_parser_t *Operator  = mpc_new("operator");
   mpc_parser_t *Expr      = mpc_new("expr");
   mpc_parser_t *Lezchty   = mpc_new("lezchty");
 
   /* Define language parser */
   mpca_lang(MPCA_LANG_DEFAULT,
-    "                                                       \
-      number    :  /-?[0-9]+/ ;                             \
-      operator  :  '+' | '-' | '*' | '/' ;                  \
-      expr      : <number> | '(' <operator> <expr>+ ')' ;   \
-      lezchty   : /^/ <operator> <expr>+ /$/ ;              \
+    "                                                                 \
+      number    :  /-?[0-9]+/ ;                                       \
+      letter    :  'a' | 'b'  ;                                       \
+      operator  :  '+' | '-' | '*' | '/' | '%' ;                      \
+      expr      : <number> | <letter> | '(' <operator> <expr>+ ')' ;  \
+      lezchty   : /^/ <operator> <expr>+ /$/ ;                        \
     ",
-    Number, Operator, Expr, Lezchty);
+    Number, Letter, Operator, Expr, Lezchty);
 
 
   /* Print version and Exit information */
@@ -63,7 +98,8 @@ int main(int argc, char** argv)
     /* Parse user input */
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lezchty, &r)) {
-      mpc_ast_print(r.output);
+      long result = eval(r.output);
+      printf("%li\n", result);
       mpc_ast_delete(r.output);
     } else {
       mpc_err_print(r.error);
@@ -75,7 +111,7 @@ int main(int argc, char** argv)
   }
 
   /* Undefine and Delete Parsers */
-  mpc_cleanup(4, Number, Operator, Expr, Lezchty);
+  mpc_cleanup(5, Number, Letter, Operator, Expr, Lezchty);
 
   return 0;
 }
